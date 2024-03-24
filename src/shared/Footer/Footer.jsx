244d5098941b23ea6d786fc5d08a21c7580dd6e0
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-24 23:59:05 +0600

    footer done

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { FaMapLocationDot, FaRegHeart } from "react-icons/fa6";
+import { IoFilterSharp } from "react-icons/io5";
 
 const Footer = () => {
-    return (
-        <div className='w-full h-16 text-gray-600 bg-black fixed bottom-0 left-0'>
-            <h1 className='text-center'>This is footer</h1>
-        </div>
-    );
+  return (
+    <div className="lg:hidden w-full flex justify-between h-16 p-5 text-gray-600 bg-white fixed bottom-0 left-0">
+      <div className="flex gap-10 text-2xl text-[#037092]">
+        <FaMapLocationDot />
+        <FaRegHeart />
+      </div>
+      <div className="flex items-center gap-2 text-[#037092]">
+        <IoFilterSharp />
+        <h1>Filters</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
