e29354e292bcd4d8322ad6ae19c28c83ac88200f
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-24 14:22:36 +0600

    header setup

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,3 +2,18 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+import Main from "../layout/Main/Main";
+import Home from "../pages/Home/Home";
+
+ export  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        }
+      ],
+    },
+  ]);
