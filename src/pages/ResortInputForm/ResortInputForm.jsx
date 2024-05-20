db270d17f298463080e2621b7573f6512c774824
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-05-20 21:12:38 +0600

    new update on form

--- src/pages/ResortInputForm/ResortInputForm.jsx
+++ src/pages/ResortInputForm/ResortInputForm.jsx
@@ -296,7 +296,8 @@ const ResortInputForm = () => {
             <option value="13.00">13.00</option>
             <option value="14.00">14.00</option>
             <option value="15.00">15.00</option>
-            <option value="16.00">16.00</option> {/* Fixed the value here */}
+            <option value="16.00">16.00</option> 
+            <option value="17.00">17.00</option> 
           </select>
         </div>
 
@@ -318,7 +319,9 @@ const ResortInputForm = () => {
           >
             <option value="">Select the Time</option>
             <option value="09.00">09.00</option>
+            <option value="09.30">09.30</option>
             <option value="10.00">10.00</option>
+            <option value="10.30">10.30</option>
             <option value="11.00">11.00</option>
             <option value="12.00">12.00</option> {/* Fixed the value here */}
           </select>
