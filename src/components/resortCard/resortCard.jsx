1ba407dbaf76608ca62d03f6a799d3e0a221915d
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-07-02 15:23:37 +0600

    new pudate for wyndham

--- src/components/resortCard/resortCard.jsx
+++ src/components/resortCard/resortCard.jsx
@@ -1,54 +1,55 @@
-import { GiStarsStack } from "react-icons/gi";
 import { FaRegHeart } from "react-icons/fa";
 
-const resortCard = ({ resort }) => {
-    const { img,  place_name,reviews_amount,location, ownerExclusive
-    } = resort;
-    
-    return (
-        <div>
-            <div className=" card h-full bg-white shadow-xl">
-                <figure className=" relative">
-                    <img src={img} alt="Shoes" />
-                    <p className="absolute top-5 right-5 text-xl"><FaRegHeart /></p>
-                {/* <div className= "absolute flex flex-col items-center bottom-5 right-5 p-2 rounded bg-white">
-                    <p className="">Starting At </p>
-                    <p className="text-lg font-bold"> ${price_usd} USD</p>
-                </div> */}
-                    </figure>
-                
-                <div className="card-body">
-                <p>{location}</p>
-                    <h2 className="card-title">
-                        {place_name}
-                        
-                    </h2>
-                <div>
-                    <div className="flex items-center gap-2">
-                    {/* <GiStarsStack /> */}
-                    <h1>{ownerExclusive}</h1>
-                    </div>
+const ResortCard = ({ resort }) => {
+  const { img, place_name, reviews_amount, location, ownerExclusive } = resort;
 
-                        <div className="divider"></div>
+  return (
+    <div>
+      <div className="card h-full bg-white shadow-xl">
+        <figure className="relative">
+          <img src={img} alt="Resort" />
+          <p className="absolute top-5 right-5 text-xl">
+            <FaRegHeart />
+          </p>
+        </figure>
 
-                    <div className="flex items-center gap-1">
-                        <img src="https://clubs.rci.com/static/media/gold-crown.d40b5cfc.svg" alt="" />
-                        <div className="border-l-2 "></div>
+        <div className="card-body">
+          <p>{location}</p>
+          <h2 className="card-title">{place_name}</h2>
 
-                        <div className="">
-                        <img src="https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/5.0-32772-5.svg" alt="" />
-                            {/* You need to add reviews_amount in resort object */}
-                            <div>
-                                <p>{reviews_amount} reviews</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
+          <div className="flex items-center gap-2">
+            <h1>{ownerExclusive}</h1>
+          </div>
+
+          {place_name.includes("Wyndham") && (
+            <div className="flex items-center gap-2">
+              <h1>Wyndham owner exclusive</h1>
             </div>
-            
+          )}
+
+          <div className="divider"></div>
+
+          <div className="flex items-center gap-1">
+            <img
+              src="https://clubs.rci.com/static/media/gold-crown.d40b5cfc.svg"
+              alt="Gold Crown"
+            />
+            <div className="border-l-2 "></div>
+
+            <div className="">
+              <img
+                src="https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/5.0-32772-5.svg"
+                alt="TripAdvisor Rating"
+              />
+              <div>
+                <p>{reviews_amount} reviews</p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default resortCard;
+export default ResortCard;
