aac5e2c0303dd47455436efc65381f7cb2858e0d
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-09-27 22:43:08 +0600

    new update of homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import { IoSearch } from "react-icons/io5";
 import rciImg2 from "../../assets/Images/rci-magazine-people-places.jpg";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div >
+    <div>
       <div className="relative flex justify-center h-[660px] lg:h-[350px] lg:w-full lg:bg-center  bg-no-repeat bg-[url('https://www.rci.com/static/images/content/_NAMER/clubs/wvr-hero1.jpg?impolicy=club-570-760')] lg:bg-[url('https://www.rci.com/static/images/content/_NAMER/clubs/wvr/wvr-hero4-desktop-500.jpg')]">
         <div className="absolute top-[400px] lg:top-[150px] lg:left-16 px-5 py-8 rounded-3xl space-y-3 bg-white">
           <div className="flex justify-center ">
@@ -21,29 +22,52 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="lg:container lg:mx-auto text-center space-y-5  border my-10 p-4">
-        <div className="lg:grid lg:grid-cols-2 md:space-x-5 ">
-        <div className="md:text-right md:p-5">
-        <h1 className="text-3xl font-normal md:font-semibold mb-2">
-          You have ZERO points in your RCI account!
-        </h1>
-        <p className="text-base">
-          Before you can book a vacation with RCI, you must have sufficient
-          Points in your RCI account. Get inspired by searching available RCI
-          vacations, then return to your Club Wyndham account to deposit the
-          number of Points required for the trip you'd like to book.
-        </p>
+      <div>
+        <div className="flex flex-col items-center py-20 bg-[#fbf3ec]">
+          <img
+            src="https://clubs.rci.com/static/media/mi.d7834b71.svg"
+            alt=""
+            className=""
+          />
         </div>
 
-        <div className="p-5 shadow-md border space-y-2 lg:rounded-none rounded-3xl my-5">
-          <h1 className="text-xl font-semibold">
-            Get Points for your next vacation
-          </h1>
-          <p>by depositing your Club Wyndham Points with RCI</p>
-          <button className="btn text-black font-bold shadow-md  bg-[#ffcc45] ">
-            DEPOSIT MY POINTS
-          </button>
+        <div className="w-full py-5 ">
+          <div className="px-5">
+          <h1 className="text-xl font-bold">Clubs Targeted 25% off exchange test</h1>
+          <p>25% Off exchange test</p>
+          </div>
+
+          <div className=" flex flex-col items-center form-control mt-6">
+            <Link className="w-11/12">
+            <button className="w-11/12 rounded border-2 text-[#037092] border-[#037092] py-2 text-lg font-semibold shadow-md">Learn More</button>
+            </Link>
+          </div>
         </div>
+      </div>
+
+      <div className="lg:container lg:mx-auto text-center space-y-5  border my-10 p-4">
+        <div className="lg:grid lg:grid-cols-2 md:space-x-5 ">
+          <div className="md:text-right md:p-5">
+            <h1 className="text-3xl font-normal md:font-semibold mb-2">
+              You have ZERO points in your RCI account!
+            </h1>
+            <p className="text-base">
+              Before you can book a vacation with RCI, you must have sufficient
+              Points in your RCI account. Get inspired by searching available
+              RCI vacations, then return to your Club Wyndham account to deposit
+              the number of Points required for the trip you'd like to book.
+            </p>
+          </div>
+
+          <div className="p-5 shadow-md border space-y-2 lg:rounded-none rounded-3xl my-5">
+            <h1 className="text-xl font-semibold">
+              Get Points for your next vacation
+            </h1>
+            <p>by depositing your Club Wyndham Points with RCI</p>
+            <button className="btn text-black font-bold shadow-md  bg-[#ffcc45] ">
+              DEPOSIT MY POINTS
+            </button>
+          </div>
         </div>
 
         <div className="w-full lg:grid lg:grid-cols-2 bg-[#e6f8fc] my-10 ">
